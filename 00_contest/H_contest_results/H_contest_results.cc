b875bc7bc62cfb78fde21f0c841b00a8d5bdbc39
Tagir Enikeev <enikeev.tg@gmail.com>
2023-11-19 23:33:18 +0300

    contest: task H round up

--- 00_contest/H_contest_results/H_contest_results.cc
+++ 00_contest/H_contest_results/H_contest_results.cc
@@ -1,15 +1,21 @@
 #include <iostream>
 
 int main() {
-  int solved_first_group = 0;
-  int solved_second_group = 0;
+  int solved_group_1 = 0;
+  int solved_group_2 = 0;
   int tasks_number = 0;
-  std::cin >> solved_first_group >> solved_second_group >> tasks_number;
+  std::cin >> solved_group_1 >> solved_group_2 >> tasks_number;
 
-  int max_first_group = solved_first_group;
-  int min_second_group = solved_second_group / tasks_number;
-  if (solved_second_group % tasks_number > 0) ++min_second_group;
-  if (max_first_group > min_second_group) {
+  int group_1_max = solved_group_1;
+
+  // деление с округлением вверх 1:
+  // int group_2_min = (solved_group_2 + tasks_number - 1) /
+  // tasks_number; if (solved_group_2 % tasks_number > 0)
+  // ++group_2_min;
+  // деление с округлением вверх 2:
+  int group_2_min = (solved_group_2 + tasks_number - 1) / tasks_number;
+
+  if (group_1_max > group_2_min) {
     std::cout << "Yes";
   } else {
     std::cout << "No";
