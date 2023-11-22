a4b3a06856188f394eab95dbeae257689fbd37d0
Tagir Enikeev <enikeev.tg@gmail.com>
2023-11-22 11:35:48 +0300

    day01: task B: quicksort works

--- 01_sorts/B_quick_sort/B_quick_sort.cc
+++ 01_sorts/B_quick_sort/B_quick_sort.cc
@@ -2,18 +2,43 @@
  * QuickSort:
  *
  * Алгоритм быстрой сортировки реализован в методе QuickSort()
- * класса QSArray. Метод QuickSort() основан на методе Partition() из задания A
+ * класса QSArray. Метод QuickSort(), вызывающий QuickSort(begin, end),
+ * который в свою очередь вызывает метод Partition(begin, end, pivot)
+ * из задания A. Далее идёт рекрсивный вызов QuickSort(begin, end)
  *
- * В классе QSArray реализован шаблонные методы FindFirstElem() и
- * FindLastElem(), возвращающие итератор (указатель) на первый
+ * Метод Partition() использует методы с функциональными объектами-предикатами,
+ * которые реализованы в виде шаблонных методов FindFirstElem() и
+ * FindLastElem(). Эти методы возвращают итератор (указатель) на первый
  * и последний элемент массива соответственно (начиная с указателя на
  * некоторый элемент) удовлетворяющий условию предиката
  *
- * Предикаты выполнен в виде функциональных объектов классов LessThanPivot и
+ * Предикаты выполнены в виде функциональных объектов классов LessThanPivot и
  * GreaterThanOrEqualToPivot
  **/
 
 #include <iostream>
+
+// Functor: Less Than Pivot
+class LessThanPivot {
+ public:
+  LessThanPivot(const int& pivot_value) : pivot_(pivot_value) {}
+
+  bool operator()(const int& value) { return value < pivot_; }
+
+ private:
+  int pivot_{0};
+};
+
+// Functor: Greater Than Or Equal To Pivot
+class GreaterThanOrEqualToPivot {
+ public:
+  GreaterThanOrEqualToPivot(const int& pivot_value) : pivot_(pivot_value) {}
+
+  bool operator()(const int& value) { return value >= pivot_; }
+
+ private:
+  int pivot_{0};
+};
 
 class QSArray {
  public:
@@ -28,14 +53,8 @@ class QSArray {
   }
 
   void PrintArray() {
-    int cols = 0;
     for (int i = 0; i < size_; ++i) {
       std::cout << array_[i] << ' ';
-      ++cols;
-      if (cols == 5) {
-        std::cout << '\n';
-        cols = 0;
-      }
     }
     std::cout << std::endl;
   }
@@ -50,35 +69,50 @@ class QSArray {
     *second = tmp;
   }
 
-  // int* Partition(int* begin, int* end, const int& pivot) {
-  //   GreaterThanOrEqualToPivot pred_greater_or_equal(pivot);
-  //   LessThanPivot pred_less(pivot);
+  template <typename predicate_functor>
+  int* FindFirstElem(int* begin, int* end, predicate_functor predicate) {
+    while (begin != end && predicate(*begin) == false) ++begin;
+    if (begin == end) begin = nullptr;
+    return begin;
+  }
 
-  //   int* ptr_greater_or_equal = begin;
-  //   int* ptr_less = end;
-  //   while (ptr_greater_or_equal && ptr_less &&
-  //          ptr_greater_or_equal < ptr_less) {
-  //     ptr_greater_or_equal = FindFirstElem(begin, end,
-  //     pred_greater_or_equal); ptr_less = FindLastElem(begin, end, pred_less);
+  template <typename predicate_functor>
+  int* FindLastElem(int* begin, int* end, predicate_functor predicate) {
+    --end;
+    while (end != begin && predicate(*end) == false) --end;
+    if (end == begin && predicate(*end) == false) end = nullptr;
+    return end;
+  }
 
-  //     if (ptr_greater_or_equal && ptr_less) {
-  //       if (ptr_greater_or_equal < ptr_less) {
-  //         Swap(ptr_greater_or_equal, ptr_less);
-  //       }
-  //       begin = ptr_greater_or_equal;
-  //       end = ptr_less;
-  //     } else if (!ptr_greater_or_equal && ptr_less) {
-  //       begin = ptr_less + 1;
-  //     }
-  //   }
+  int* Partition(int* begin, int* end, const int& pivot) {
+    GreaterThanOrEqualToPivot pred_greater_or_equal(pivot);
+    LessThanPivot pred_less(pivot);
 
-  //   return begin;
-  // }
+    int* ptr_greater_or_equal = begin;
+    int* ptr_less = end;
+    while (ptr_greater_or_equal && ptr_less &&
+           ptr_greater_or_equal < ptr_less) {
+      ptr_greater_or_equal = FindFirstElem(begin, end,
+      pred_greater_or_equal); ptr_less = FindLastElem(begin, end, pred_less);
 
-  // int* RandPartition(int* begin, int* end) {
-  //   int pivot = *(begin + std::rand() % (end - begin));
-  //   return Partition(begin, end, pivot);
-  // }
+      if (ptr_greater_or_equal && ptr_less) {
+        if (ptr_greater_or_equal < ptr_less) {
+          Swap(ptr_greater_or_equal, ptr_less);
+        }
+        begin = ptr_greater_or_equal;
+        end = ptr_less;
+      } else if (!ptr_greater_or_equal && ptr_less) {
+        begin = ptr_less + 1;
+      }
+    }
+
+    return begin;
+  }
+
+  int* RandPartition(int* begin, int* end) {
+    int pivot = *(begin + std::rand() % (end - begin));
+    return Partition(begin, end, pivot);
+  }
 
   void QuickSort() {
     if (size_ > 1) QuickSort(array_, array_ + size_);
@@ -86,27 +120,9 @@ class QSArray {
 
   void QuickSort(int* begin, int* end) {
     int pivot = *(begin + std::rand() % (end - begin));
-
-    int* ptr_greater_or_equal = begin;
-    int* ptr_less = end - 1;
-    int* bound = nullptr;  // right part begin (left part afterlast element)
-    while (ptr_greater_or_equal < ptr_less) {
-      while (ptr_greater_or_equal < ptr_less && *ptr_greater_or_equal < pivot) {
-        ++ptr_greater_or_equal;
-      }
-      while (ptr_less > ptr_greater_or_equal && *ptr_less >= pivot) {
-        --ptr_less;
-      }
-
-      if (ptr_greater_or_equal < ptr_less) {
-        Swap(ptr_greater_or_equal, ptr_less);
-      } else {
-        bound = ptr_less;
-      }
-    }
-
-    if (bound == begin && *(bound + 1) == *bound) bound = begin + 1;
+    int* bound = Partition(begin, end, pivot);
     if (bound - begin > 1) QuickSort(begin, bound);
+    while (bound < end && *bound == pivot) ++bound;
     if (end - bound > 1) QuickSort(bound, end);
   }
 
---
+++ 01_sorts/B_quick_sort/B_quick_sort_2.cc
@@ -0,0 +1,111 @@
+/**
+ * QuickSort:
+ *
+ * Алгоритм быстрой сортировки реализован в методе QuickSort()
+ * класса QSArray. Метод QuickSort(), вызывающий QuickSort(begin, end).
+ * Далее идёт рекрсивный вызов QuickSort(begin, end)
+ **/
+
+#include <iostream>
+
+class QSArray {
+ public:
+  QSArray() {}
+  QSArray(size_t size) : size_(size) { array_ = new int[size_]{}; }
+  ~QSArray() { delete[] array_; }
+
+  void FillArray() {
+    for (int i = 0; i < size_; ++i) {
+      std::cin >> array_[i];
+    }
+  }
+
+  void PrintArray() {
+    for (int i = 0; i < size_; ++i) {
+      std::cout << array_[i] << ' ';
+    }
+  }
+
+  int* Begin() { return array_; }
+
+  int* End() { return array_ + size_; }
+
+  void Swap(int* first, int* second) {
+    int tmp = *first;
+    *first = *second;
+    *second = tmp;
+  }
+
+  // int* Partition(int* begin, int* end, const int& pivot) {
+  //   GreaterThanOrEqualToPivot pred_greater_or_equal(pivot);
+  //   LessThanPivot pred_less(pivot);
+
+  //   int* ptr_greater_or_equal = begin;
+  //   int* ptr_less = end;
+  //   while (ptr_greater_or_equal && ptr_less &&
+  //          ptr_greater_or_equal < ptr_less) {
+  //     ptr_greater_or_equal = FindFirstElem(begin, end,
+  //     pred_greater_or_equal); ptr_less = FindLastElem(begin, end, pred_less);
+
+  //     if (ptr_greater_or_equal && ptr_less) {
+  //       if (ptr_greater_or_equal < ptr_less) {
+  //         Swap(ptr_greater_or_equal, ptr_less);
+  //       }
+  //       begin = ptr_greater_or_equal;
+  //       end = ptr_less;
+  //     } else if (!ptr_greater_or_equal && ptr_less) {
+  //       begin = ptr_less + 1;
+  //     }
+  //   }
+
+  //   return begin;
+  // }
+
+  // int* RandPartition(int* begin, int* end) {
+  //   int pivot = *(begin + std::rand() % (end - begin));
+  //   return Partition(begin, end, pivot);
+  // }
+
+  void QuickSort() {
+    if (size_ > 1) QuickSort(array_, array_ + size_);
+  }
+
+  void QuickSort(int* begin, int* end) {
+    int pivot = *(begin + std::rand() % (end - begin));
+
+    int* ptr_greater_or_equal = begin;
+    int* ptr_less = end - 1;
+    int* bound = nullptr;  // right part begin (left part afterlast element)
+    while (ptr_greater_or_equal < ptr_less) {
+      while (ptr_greater_or_equal < ptr_less && *ptr_greater_or_equal < pivot) {
+        ++ptr_greater_or_equal;
+      }
+      while (ptr_less > ptr_greater_or_equal && *ptr_less >= pivot) {
+        --ptr_less;
+      }
+
+      if (ptr_greater_or_equal < ptr_less) {
+        Swap(ptr_greater_or_equal, ptr_less);
+      } else {
+        bound = ptr_less;
+      }
+    }
+
+    if (bound - begin > 1) QuickSort(begin, bound);
+    if (end - bound > 1) QuickSort(bound, end);
+  }
+
+ private:
+  int* array_{nullptr};
+  size_t size_{0};
+};
+
+int main() {
+  size_t array_length = 0;
+  std::cin >> array_length;
+  QSArray array(array_length);
+  array.FillArray();
+  array.QuickSort();
+  array.PrintArray();
+  return 0;
+}
